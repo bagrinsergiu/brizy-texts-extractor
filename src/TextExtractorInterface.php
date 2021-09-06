9146895ddc30a6060ef6c76f1b819e29c2ff164d
Alex Zaharia <alecszaharia@gmail.com>
2021-09-06 14:31:15 +0300

    Added return type to TextExtractorInterface

--- src/TextExtractorInterface.php
+++ src/TextExtractorInterface.php
@@ -3,7 +3,17 @@ namespace BrizyTextsExtractor;
 
 interface TextExtractorInterface
 {
+    /**
+     * @param $content
+     *
+     * @return array<ExtractedContent>
+     */
     public function extractFromContent($content): array;
 
+    /**
+     * @param $url
+     *
+     * @return array<ExtractedContent>
+     */
     public function extractFromUrl($url): array;
 }
